f938f3ab163209bae7b0a2fc921a6eb027ed5b72
Michael Buffone <m_buffone@hotmail.com>
2022-02-16 20:44:37 -0500

    fixed a error in my comments to is a number

--- timer1.js
+++ timer1.js
@@ -2,10 +2,10 @@
 const timerArr = process.argv.slice(2);
 
 timerArr.forEach(time => {
-  //will only run if the number is greater than 0 and isNan
+  //will only run if the number is greater than 0 and is a number
   if(time > 0) {
     setTimeout(() => {
-      console.log(`Beep at ${time} seconds.`)
+      console.log(`Beeps at ${time} seconds.`)
       process.stdout.write('\x07'); //creates a beep sound on the time its comes up
     }, time * 1000); // sets the beep for what time is added * 1 sec
   }
